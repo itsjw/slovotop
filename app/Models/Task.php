03e648d98724f502c551e9a765fe16f3aa320064
soeOleg <soe190715@gmail.com>
2017-11-20 17:09:55 +0700

    model task relation

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -14,11 +14,63 @@ class Task extends Model
 {
 
     /**
-     * @return array
+     * @var array
      */
-    public function getTableColumns()
+    protected $dates = ['date_end'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user()
     {
-        return $this->getConnection()->getSchemaBuilder()->getColumnListing($this->getTable());
+        return $this->hasOne(User::class, 'id', 'user_id');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function project()
+    {
+        return $this->hasOne(Project::class, 'id', 'project_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function status()
+    {
+        return $this->hasOne(TaskStatus::class, 'id', 'status_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function stage()
+    {
+        return $this->hasOne(TaskStage::class, 'id', 'stage_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function editor()
+    {
+        return $this->hasOne(User::class, 'id', 'editor_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function author()
+    {
+        return $this->hasOne(User::class, 'id', 'author_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function subject()
+    {
+        return $this->hasOne(TaskSubject::class, 'id', 'subject_id');
+    }
 }
