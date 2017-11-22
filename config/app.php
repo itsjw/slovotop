5fabd9aa2c86bcbef6d64872c93036aa2624fdd0
soeOleg <soe190715@gmail.com>
2017-11-22 15:01:53 +0700

    remove graphql

--- config/app.php
+++ config/app.php
@@ -177,9 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        // GraphQL
-        Rebing\GraphQL\GraphQLServiceProvider::class,
-
         // dev
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
@@ -231,9 +228,6 @@ return [
         'URL'          => Illuminate\Support\Facades\URL::class,
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
-
-        // GraphQL
-        'GraphQL'      => Rebing\GraphQL\Support\Facades\GraphQL::class,
     ],
 
 ];
