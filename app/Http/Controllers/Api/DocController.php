4ae0e044692b3e0de4951c99b5030dbbff70051a
soeOleg <soe190715@gmail.com>
2017-11-28 10:30:49 +0700

    api save and delete doc

--- app/Http/Controllers/Api/DocController.php
+++ app/Http/Controllers/Api/DocController.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\DocSaveValidation;
 use App\Models\Doc;
+use App\Models\Role;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Docs\Doc as DocResource;
@@ -28,5 +30,44 @@ class DocController extends Controller
         }
 
         return DocResource::collection($doc->get());
+    }
+
+    /**
+     * @param DocSaveValidation $request
+     *
+     * @return array
+     */
+    public function saveDoc(DocSaveValidation $request)
+    {
+        $doc = Doc::findOrNew($request->id);
+
+        $doc->name = $request->name;
+        $doc->body = $request->body;
+        $doc->user_id = $request->user;
+        $doc->save();
+        $doc->roles()->detach();
+
+        foreach ($request->roles as $val) {
+            $role = Role::find($val);
+            $doc->roles()->save($role, ['access' => 1]);
+        }
+
+        return ['success' => trans('data.notifyOK')];
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function deleteDoc(Request $request)
+    {
+        foreach ($request->items as $item) {
+            $doc = Doc::findOrfail($item);
+            $doc->roles()->detach();
+            $doc->delete();
+        }
+
+        return ['success' => trans('data.notifyOK')];
     }
 }
