0f6c7e7b3615162234fd4c416bd2cc86b981fd78
soeOleg <soe190715@gmail.com>
2017-12-04 17:10:04 +0700

    get task - access date

--- app/Http/Resources/Task/Task.php
+++ app/Http/Resources/Task/Task.php
@@ -26,45 +26,84 @@ class Task extends Resource
         $stageTask = StageTaskAccess::where('stage_id', $this->stage_id)->select('field', 'access')->get();
 
         return [
-            'id'           => $this->id,
-            'owner'        => new UserLittle($this->user),
-            'name'         => [
+            'id'          => $this->id,
+            'owner'       => new UserLittle($this->user),
+            'name'        => [
                 'data'   => $this->name,
                 'access' => $stageTask->where('field', 'name')->first()->access ?? 0,
             ],
-            'project'      => [
+            'project'     => [
                 'data'   => new ProjectLittle($this->editor),
                 'access' => $stageTask->where('field', 'project_id')->first()->access ?? 0,
             ],
-            'status'       => $this->status_id,
-            'stage'        => $this->stage_id,
-            'editor'       => [
+            'status'      => $this->status_id,
+            'stage'       => $this->stage_id,
+            'editor'      => [
                 'date'   => new ProjectLittle($this->user),
                 'access' => $stageTask->where('field', 'editor_id')->first()->access ?? 0,
             ],
-            'author'       => [
+            'author'      => [
                 'data'   => new ProjectLittle($this->author),
                 'access' => $stageTask->where('field', 'author_id')->first()->access ?? 0,
             ],
-            'subject'      => [
+            'subject'     => [
                 'data'   => $this->subject,
                 'access' => $stageTask->where('field', 'subject_id')->first()->access ?? 0,
             ],
-            'date_end'     => '',
-            'price'        => '',
-            'title'        => '',
-            'desc'         => '',
-            'words'        => '',
-            'more_data'    => '',
-            'task'         => '',
-            'text_body'    => '',
-            'text_preview' => '',
-            'text_url'     => '',
-            'text_min'     => '',
-            'text_max'     => '',
-            'text_unique'  => '',
-            'created_at'   => $this->created_at->format('d-m-Y H:m:s'),
-            'updated_at'   => $this->updated_at->format('d-m-Y H:m:s'),
+            'dateEnd'     => [
+                'data'   => $this->date_end->format('d-m-Y'),
+                'access' => $stageTask->where('field', 'date_end')->first()->access ?? 0,
+            ],
+            'price'       => [
+                'data'   => $this->price,
+                'access' => $stageTask->where('field', 'price')->first()->access ?? 0,
+            ],
+            'title'       => [
+                'data'   => $this->title,
+                'access' => $stageTask->where('field', 'title')->first()->access ?? 0,
+            ],
+            'desc'        => [
+                'data'   => $this->desc,
+                'access' => $stageTask->where('field', 'desc')->first()->access ?? 0,
+            ],
+            'words'       => [
+                'data'   => $this->words,
+                'access' => $stageTask->where('field', 'words')->first()->access ?? 0,
+            ],
+            'moreData'    => [
+                'data'   => $this->more_data,
+                'access' => $stageTask->where('field', 'more_data')->first()->access ?? 0,
+            ],
+            'task'        => [
+                'data'   => $this->task,
+                'access' => $stageTask->where('field', 'task')->first()->access ?? 0,
+            ],
+            'textBody'    => [
+                'data'   => $this->text_body,
+                'access' => $stageTask->where('field', 'text_body')->first()->access ?? 0,
+            ],
+            'textPreview' => [
+                'data'   => $this->text_preview,
+                'access' => $stageTask->where('field', 'text_preview')->first()->access ?? 0,
+            ],
+            'textUrl'     => [
+                'data'   => $this->text_url,
+                'access' => $stageTask->where('field', 'text_url')->first()->access ?? 0,
+            ],
+            'textMin'     => [
+                'data'   => $this->text_min,
+                'access' => $stageTask->where('field', 'text_min')->first()->access ?? 0,
+            ],
+            'textMax'     => [
+                'data'   => $this->text_max,
+                'access' => $stageTask->where('field', 'text_max')->first()->access ?? 0,
+            ],
+            'textUnique'  => [
+                'data'   => $this->text_unique,
+                'access' => $stageTask->where('field', 'text_unique')->first()->access ?? 0,
+            ],
+            'created_at'  => $this->created_at->format('d-m-Y H:m:s'),
+            'updated_at'  => $this->updated_at->format('d-m-Y H:m:s'),
         ];
     }
 }
