6d1e725b15a5aee8b666a9dbecc7f8ca8440a856
soeOleg <soe190715@gmail.com>
2017-12-11 09:31:15 +0700

    add mdicon

--- resources/views/crm/head/head.blade.php
+++ resources/views/crm/head/head.blade.php
@@ -2,17 +2,17 @@
 
     <div class="navbar-start">
         <div class="navbar-item">
-            <h1 class="title is-5">@yield('HeadTitle')</h1>
+            <h1 class="title is-2">@yield('HeadTitle')</h1>
         </div>
     </div>
 
     <div class="navbar-end">
         <div class="navbar-item">
             <a href="{{ route('logout') }}" class="button is-info">
-                <span class="icon">
-                    <i class="fa fa-sign-out"></i>
-                </span>
                 <span>{{ \Auth::user()->name }}</span>
+                <span class="icon">
+                    <i class="mdi mdi-logout"></i>
+                </span>
             </a>
         </div>
     </div>
